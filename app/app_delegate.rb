1cdd5f7669c8c72a027cb904036b61f51410a16e
Gant <GantMan@gmail.com>
2015-03-26 15:08:57 -0500

    began concierge

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,7 +5,19 @@ class AppDelegate < PM::Delegate
 
   def on_load(app, options)
     cdq.setup
+
+    # Set up motion-concierge
+    MotionConcierge.local_file_name = 'seafood_data.json'
+    # TODO
+    MotionConcierge.remote_file_url = 'https://raw.github.com/IconoclastLabs/rubytrivia/master/resources/qa.json'
+    MotionConcierge.fetch_interval = 86400 # Once a day
+    
     open HomeScreen.new(nav_bar: true)
+  end
+
+  # Check for the data each time the app is launched or returns active
+  def on_activate
+    MotionConcierge.fetch
   end
 
   # Remove this if you are only supporting portrait
