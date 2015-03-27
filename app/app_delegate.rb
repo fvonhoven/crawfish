179586168dff9a9a7c9ff154c188328bc8728422
Gant <GantMan@gmail.com>
2015-03-26 22:17:43 -0500

    using json to feed the data

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -6,18 +6,18 @@ class AppDelegate < PM::Delegate
   def on_load(app, options)
     cdq.setup
 
-    # Set up motion-concierge
-    MotionConcierge.local_file_name = 'seafood_data.json'
-    # TODO
-    MotionConcierge.remote_file_url = 'https://raw.github.com/IconoclastLabs/rubytrivia/master/resources/qa.json'
-    MotionConcierge.fetch_interval = 86400 # Once a day
+    # # Set up motion-concierge
+    # MotionConcierge.local_file_name = 'seafood_data.json'
+    # # TODO
+    # MotionConcierge.remote_file_url = 'https://raw.github.com/IconoclastLabs/rubytrivia/master/resources/qa.json'
+    # MotionConcierge.fetch_interval = 86400 # Once a day
     
     open HomeScreen.new(nav_bar: true)
   end
 
   # Check for the data each time the app is launched or returns active
   def on_activate
-    MotionConcierge.fetch
+    # MotionConcierge.fetch
   end
 
   # Remove this if you are only supporting portrait
--- app/screens/finder_screen.rb
+++ app/screens/finder_screen.rb
@@ -91,11 +91,26 @@ class FinderScreen < PM::TableScreen
         # },
       ]
     }]
+
+    #unless json_string = MotionConcierge.local_file_string
+      mp "My data came from the local copy"
+      seed_file = NSBundle.mainBundle.pathForResource('seafood', ofType:'json')
+      json_string = String.new(NSString.stringWithContentsOfFile(seed_file))
+    #end
+    @data = [parse(json_string)]
+
   end
 
   def table_data
     @data
   end
+
+  def parse(str_data)
+    data = str_data.dataUsingEncoding(NSUTF8StringEncoding)
+    opts = NSJSONReadingMutableContainers | NSJSONReadingMutableLeaves | NSJSONReadingAllowFragments
+    error = Pointer.new(:id)
+    NSJSONSerialization.JSONObjectWithData(data, options:opts, error:error)
+  end  
 
   def tap_company company
     #value = state.each_value {|value| p "VISIT #{value.upcase} MO FUCKA"}
